4c978d649e5b9bb4f8162353b0a064a28ce2b492
Brian Pugh <bnp117@gmail.com>
2023-07-02 21:02:24 -0700

    move out of GITHUB_WORKSPACE

--- index.js
+++ index.js
@@ -6,30 +6,27 @@ const io = require('@actions/io');
 async function run() {
   // Get the repository URL and reference
   const repository = core.getInput('repository');
-  const reference = core.getInput('reference');
+  let reference = core.getInput('reference');
+
+  const mpy_dir = '/home/runner/micropython'
+  core.exportVariable('MPY_DIR', mpy_dir);
 
   // Shallow clone the repository to get the latest commit hash or checkout the provided reference
-  await exec.exec(`git clone --depth 1 ${repository} micropython`);
+  await exec.exec(`git clone --depth 1 ${repository} ${mpy_dir}`);
 
-  // Export the micropython directory
-  const mpy_path = `${process.env.GITHUB_WORKSPACE}/micropython`
-  core.exportVariable('MPY_DIR', mpy_path);
-
-  let ref = reference;
-
-  if(ref){
-    await exec.exec(`git checkout ${ref}`, [], {cwd: 'micropython'});
+  if(reference){
+    await exec.exec(`git checkout ${reference}`, [], {cwd: mpy_dir});
   }
 
-  const logResult = await exec.getExecOutput('git', ['rev-parse', 'HEAD'], {cwd: 'micropython'});
-  ref = logResult.stdout.trim();
+  const logResult = await exec.getExecOutput('git', ['rev-parse', 'HEAD'], {cwd: mpy_dir});
+  reference = logResult.stdout.trim();
 
-  const cacheKey = `micropython-unix-port-${repository}-${ref}`;
+  const cacheKey = `micropython-unix-port-${repository}-${reference}`;
 
   const cachePaths = [
     '/usr/local/bin/micropython',
     '/usr/local/bin/mpy-cross',
-    mpy_path
+    mpy_dir
   ]
   const cacheHit = await cache.restoreCache(cachePaths, cacheKey);
 
@@ -38,15 +35,15 @@ async function run() {
   }
 
   // Build mpy-cross
-  await exec.exec('bash', ['-c', 'make -j$(nproc)'], {cwd: 'micropython/mpy-cross'});
+  await exec.exec('bash', ['-c', 'make -j$(nproc)'], {cwd: `${mpy_dir}/mpy-cross`});
 
   // Build Unix Port
-  await exec.exec('make submodules', [], {cwd: 'micropython/ports/unix'});
-  await exec.exec('bash', ['-c', 'make -j$(nproc)'], {cwd: 'micropython/ports/unix'});
+  await exec.exec('make submodules', [], {cwd: `${mpy_dir}/ports/unix`});
+  await exec.exec('bash', ['-c', 'make -j$(nproc)'], {cwd: `${mpy_dir}/ports/unix`});
 
   // Add the built binaries to the PATH
-  await io.cp('micropython/mpy-cross/build/mpy-cross', '/usr/local/bin/mpy-cross');
-  await io.cp('micropython/ports/unix/build-standard/micropython', '/usr/local/bin/micropython');
+  await io.cp(`${mpy_dir}/mpy-cross/build/mpy-cross`, '/usr/local/bin/mpy-cross');
+  await io.cp(`${mpy_dir}/ports/unix/build-standard/micropython`, '/usr/local/bin/micropython');
 
   // Save the cache
   await cache.saveCache(cachePaths, cacheKey);
